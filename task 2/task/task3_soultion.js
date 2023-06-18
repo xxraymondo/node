dec5213c8b06e8482c8f276584ef2baf4149f3b3
khaled yehia <63373489+xxraymondo@users.noreply.github.com>
2023-06-18 15:41:22 +0300

    task 3

---
+++ task 2/task/task3_soultion.js
@@ -0,0 +1,112 @@
+const { Console } = require('console');
+const http= require("http");
+const _fetch = (url) => fetch(url).then(res => res.json())
+
+const groupWithCategories=(products)=>{
+    const categories={}
+    products.forEach(element => {
+        if(categories[element.category.id]){
+            categories[element.category.id].products.push(element)
+        }else{
+            categories[element.category.id]={
+                category:{
+                    id:element.category.id,
+                    name:element.category.name
+                },
+                products:[element]
+            }
+        }
+    });
+    return Object.values(categories)
+}
+
+
+const categorizedProducts=async()=>{
+    const [products, rates]= await Promise.all([
+        _fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=50'),
+        _fetch('https://api.exchangerate.host/latest?base=usd').then((res)=>{
+            const readline = require('readline').createInterface({
+                input: process.stdin,
+                output: process.stdout,
+              });
+              console.log(res.rates)
+              readline.question(`what is the currency that you want to covert to in capital letters like the previous list?`, currncy => {
+                // console.log(res.rates[currncy]) 
+                let myCurrncy=parseInt(res.rates[currncy])
+               let newProducts= products.map(element=>({...element,price:element.price*myCurrncy
+                    
+                }))
+                console.log(newProducts)
+                readline.close();
+              });
+        }
+        ).catch((err)=>console.log(err))
+    ]) 
+    const categorizedProducts= groupWithCategories(products)
+    
+}
+// categorizedProducts()
+
+todos=[]
+const requestListener =(req,res)=>{
+    const _fetch = (url) => fetch(url).then(res => res.json())
+
+    const method=req.method
+    const route= req.url
+    if(method==="GET"&&route==="/"){
+    res.setHeader("content-Type","application-json")
+    res.writeHead(200)
+    res.write(JSON.stringify(todos))
+    }
+    if(method==="POST"&&route==="/"){
+        body=[]
+        req.on('data',(chunk)=>{
+            body.push(chunk)
+        }).on('end',()=>{
+           console.log(JSON.parse(Buffer.concat(body).toString())) 
+           todos.push(JSON.parse(Buffer.concat(body).toString()))
+        })
+         }
+         res.end()
+    }
+
+
+
+    // const server = http.createServer(requestListener)
+    // //listen to client requests 
+    // server.listen(3030,"localhost",()=>{
+    //     console.log("server is up and runinng")
+    // })
+
+
+    const server = http.createServer(async (req, res) => {
+        const { pathname, query } = url.parse(req.url, true);
+        if (pathname === '/products' && req.method === 'POST') {
+          let body = '';
+          req.on('data', (chunk) => {
+            body += chunk;
+          });
+      
+          req.on('end', async () => {
+            const newProduct = JSON.parse(body);
+            try {
+              // Validate the new product here...
+      
+              const response = await axios.post('https://api.escuelajs.co/api/v1/products/', newProduct);
+              const createdProduct = response.data;
+              res.writeHead(200, { 'Content-Type': 'application/json' });
+              res.end(JSON.stringify(createdProduct));
+            } catch (error) {
+              console.error('An error occurred:', error);
+              res.writeHead(500, { 'Content-Type': 'application/json' });
+              res.end(JSON.stringify({ error: 'An error occurred' }));
+            }
+          });
+        }else {
+          res.writeHead(404, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Endpoint not found' }));
+        }
+      });
+          server.listen(3030,"localhost",()=>{
+        console.log("server is up and runinng")
+    })
--- task 2/task/task_Soultion.js
+++ task 2/task/task_Soultion.js
@@ -1,14 +1,14 @@
 const { error } = require('console')
 const CC = require('currency-converter-lt')
-const test = async function(){
+const task2 = async function(){
     arr=[]
     products=[]
     categories=[]  
     finalArr=[]
+    shortFinalArr=[]
      const response = await fetch('https://api.escuelajs.co/api/v1/products');
     const data = await response.json();
     let entries =Object.entries(data) 
-
     entries.map( ([key, val] = entry) => {
         products.push(val)
         categories.push({id: val.category.id, name: val.category.name})
@@ -21,78 +21,50 @@ const test = async function(){
             }
         })
     })
-shortFinalArr=[]
 
 for(let i=finalArr.length-100;i<finalArr.length;i++){
 shortFinalArr.push(finalArr[i])
 }
 
-// // coverting currency to EGP
-shortFinalArr.map((item)=>{
-        let currencyConverter = new CC({from:"USD", to:"EGP", amount:item.price, isDecimalComma:true})  
-        currencyConverter.convert().then((response) => {
-            console.log(`item name : ${item.title} and it's price is before converting is ${item.price} and after converting is ${response}`)
-        }).catch((error)=>{
-            console.log(error)
-        })
-    })  
-}
-test()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const test = async function(){
-
-//     const response = await fetch('https://api.escuelajs.co/api/v1/products');
-//    const data = await response.json();
-//    let entries =Object.entries(data)
-//    // console.log(Object.values(dataObject))
-//    arr=[]
-//    products=[]
-//    categories=[]  
-//    finalArr=[]
-//    entries.map( ([key, val] = entry) => {
-//        products.push(val)
-//        arr.push(val.category)
-//      });
-    
-//      for(let i =0;i<arr.length;i++){
-//        categories.push({id: arr[i].id, name: arr[i].name})
-//      }
+            // // coverting currency to EGP // task 2 from node 
+            // shortFinalArr.map((item)=>{
+            //         let currencyConverter = new CC({from:"USD", to:"EGP", amount:item.price, isDecimalComma:true})  
+            //         currencyConverter.convert().then((response) => {
+            //             console.log(`item name : ${item.title} and it's price is before converting is ${item.price} and after converting is ${response}`)
+            //         }).catch((error)=>{
+            //             console.log(error)
+            //         })
+            //     })  
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
       
-//    categories.map((item)=>{
-//        finalArr.push(item)
-//        products.map((productItem)=>{
-//            if(productItem.category.id==item.id){
-//                finalArr.push(productItem)
-//            }
-//        })
-//    })
+      readline.question(`what is the currency that you want to covert to?`, currncy => {
+        shortFinalArr.map((item)=>{
+            let currencyConverter = new CC({from:"USD", to:currncy, amount:item.price, isDecimalComma:true})  
+            currencyConverter.convert().then((response) => {
+               
+            }).catch((error)=>{
+               
+                console.log(error)
+            })
+        })
+        readline.close();
+      });
 
-//       //// testing
-//    //   console.log(finalArr[622])
-//    //   console.log(finalArr[623])
+    // task 3
+}
 
 
 
-// }
-// test()
+
+
+
+
+
+
+
+
+
+
